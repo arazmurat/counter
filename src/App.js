9ca279b32f0594a576a5a82948a2a9ebae30a2bd
Arazmyrat <arazmirat@icloud.com>
2021-04-20 23:29:02 +0400

    use hooks

--- src/App.js
+++ src/App.js
@@ -1,36 +1,26 @@
-import React, { useState } from "react";
-import "./App.css";
-import ClassComp from "./ClassComponent";
-import FuncComp from "./FunctionalComponent";
+import React,{useState} from 'react'
+import './App.css';
+import ClassComp from './ClassComponent'
+import FuncComp from './FunctionalComponent'
 
 function App() {
-  const [showComponent, setShowComponent] = useState(true);
+const [isEnableCComp, setIsEnableCComp] = useState(true)
+const [isEnableFComp, setIsEnableFComp] = useState(true)
 
-  const divStyleXYZ = {
-    width: "49%",
-    float: "left",
-    height: "100vh",
-    border: "0.5px solid red",
-  };
-  const bgLeft = { backgroundColor: "#e8cebf" };
+  
+  const divStyleXYZ = {width:"49%",float:"left",height:"100vh",border:"0.5px solid red"}
+  const bgLeft ={backgroundColor:"#e8cebf"}
   //camelCase
   return (
     <div className="App">
-      <h1>{showComponent ? "Component ON" : "Component OFF"}</h1>
-      <button onClick={() => setShowComponent(!showComponent)}>
-        Component Toggle
-      </button>
-      <br />
-      {showComponent ? (
-        <div style={{ ...divStyleXYZ, ...bgLeft }}>
-          <ClassComp />
-        </div>
-      ) : (
-        ""
-      )}
-      <div style={divStyleXYZ}>
-        <FuncComp />
-      </div>
+      <h1>{isEnableCComp ? "Class Component ON" : "Class Component OFF"}
+      {isEnableFComp ? "Functional Component ON" : "Functional Component OFF"}
+      </h1>
+      <button onClick={()=>setIsEnableCComp(!isEnableCComp)}>Class Component Toggle</button>
+      <button onClick={()=>setIsEnableFComp(!isEnableFComp)}>Function Component Toggle</button>
+      <br/>
+     {isEnableCComp? <div style={{...divStyleXYZ,...bgLeft}}><ClassComp/></div>:""}
+      {isEnableFComp ? <div style={divStyleXYZ}><FuncComp/></div> : ""}
     </div>
   );
 }
